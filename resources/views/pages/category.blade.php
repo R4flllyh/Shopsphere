9c6ba144e77680c22ea862e5d05aca81cb705c69
Rafly hidayahtullah <97890868+R4flllyh@users.noreply.github.com>
2023-10-21 19:48:01 +0700

    First Commit

---
+++ resources/views/pages/category.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app', ['class' => 'g-sidenav-show bg-gray-100'])
+
+@section('content')
+    @include('layouts.navbars.auth.topnav', ['title' => 'Category'])
+    <div class="container-fluid py-4">
+        <div class="row">
+            <div class="col-md-12 mb-lg-0 mb-4">
+                <div class="card">
+                    <div class="card-body p-3">
+                        <div class="row">
+                            <div class="col-6 d-flex align-items-center">
+                                <h5 class="mb-0">🔥 Category</h5>
+                            </div>
+                            <div class="col-6 text-end">
+                                <a href="#" class="btn bg-gradient-secondary px-3 mb-0">
+                                    <i class="fas fa-plus me-2"></i> Add Category
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 mt-4">
+                <div class="card d-flex" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Category title</h5>
+                        <p class="card-text">70rb Product</p>
+                        <div class="d-flex justify-content-between gap-2">
+                            <a href="#" class="btn btn-primary mb-0 w-100">Go somewhere</a>
+                            <a href="#" class="btn btn-danger mb-0"><i class="fi fi-rr-trash"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @include('layouts.footers.auth.footer')
+    </div>
+@endsection
---
+++ resources/views/pages/product.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.app', ['class' => 'g-sidenav-show bg-gray-100'])
+
+@section('content')
+    @include('layouts.navbars.auth.topnav', ['title' => 'Product'])
+    <div class="container-fluid py-4">
+        <div class="row">
+            <div class="col-md-12 mb-lg-0 mb-4">
+                <div class="card">
+                    <div class="card-body p-3">
+                        <div class="row">
+                            <div class="col-6 d-flex align-items-center">
+                                <h5 class="mb-0">🔥 Product</h5>
+                            </div>
+                            <div class="col-6 text-end">
+                                <a href="#" class="btn bg-gradient-secondary px-3 mb-0">
+                                    <i class="fas fa-plus me-2"></i> Add Product
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 mt-4">
+                <div class="col-3">
+                    <div class="card">
+                        <div class="card-header p-0 mx-3 mt-3 position-relative z-index-1">
+                          <a href="javascript:;" class="d-block">
+                            <img src="./assets/img/kit/pro/anastasia.jpg" class="img-fluid border-radius-lg">
+                          </a>
+                        </div>
+
+                        <div class="card-body pt-2">
+                          <span class="text-gradient text-primary text-uppercase text-xs font-weight-bold my-2">Category</span>
+                          <a href="javascript:;" class="card-title h5 d-block text-darker">
+                            Product Title
+                          </a>
+                          <p class="card-description mb-4">
+                            Use border utilities to quickly style the border
+                          </p>
+                          <div class="author align-items-center justify-content-between gap-2">
+                            <a href="#" class="btn btn-primary mb-0 w-100">See Details</a>
+                            <div class="dropdown">
+                                <button class="btn btn-secondary mb-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="fi fi-rr-menu-burger text-center"></i>
+                                </button>
+                                <ul class="dropdown-menu">
+                                  <li><a class="dropdown-item" href="#">Edit</a></li>
+                                  <li><a class="dropdown-item" href="#">Delete</a></li>
+                                </ul>
+                              </div>
+                          </div>
+                        </div>
+                      </div>
+                </div>
+            </div>
+        </div>
+        @include('layouts.footers.auth.footer')
+    </div>
+@endsection
